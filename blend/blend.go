bd362ffd5fa356ee7b071f99060f81a22c3842de
Tyler Bunnell <tylerbunnell@gmail.com>
2014-02-13 14:25:04 -0700

    Initial tests working.

--- blend/blend.go
+++ blend/blend.go
@@ -4,6 +4,34 @@ const (
 	blendFunctionAdd = "+"
 )
 
-func Blend(source, dest map[string]interface{}) map[string]interface{} {
-	return nil
+var functionMap = map[string]bool{
+	blendFunctionAdd: true,
+}
+
+func keyIsFunction(key string) bool {
+	return functionMap[key]
+}
+
+func Blend(source, dest map[string]interface{}) {
+
+	for key, value := range source {
+		if _, exists := dest[key]; !exists {
+			dest[key] = make([]interface{}, 0)
+		}
+
+		for itemKey, itemValue := range value.(map[string]interface{}) {
+			if keyIsFunction(itemKey) {
+				executeFunction(itemKey, key, itemValue, dest)
+			}
+		}
+	}
+
+}
+
+func executeFunction(function, key string, value interface{}, dest map[string]interface{}) {
+	switch function {
+	case blendFunctionAdd:
+		dest[key] = append(dest[key].([]interface{}), value)
+	default:
+	}
 }
